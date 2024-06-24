987b780940192157343a06cceff256f6ffdcb0c3
Ryan Witzman <92187780+ryanwitzman@users.noreply.github.com>
2024-06-24 17:39:30 -0400

    Update trainer.py

--- FlagEmbedding/llm_reranker/finetune_for_instruction/trainer.py
+++ FlagEmbedding/llm_reranker/finetune_for_instruction/trainer.py
@@ -14,9 +14,7 @@ class BiTrainer(Trainer):
     use_lora: bool
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
-        if not self.use_lora:
-            super()._save(output_dir, state_dict)
-            return
+        super()._save(output_dir, state_dict)
 
         output_dir = output_dir if output_dir is not None else self.args.output_dir
         os.makedirs(output_dir, exist_ok=True)
@@ -29,19 +27,6 @@ class BiTrainer(Trainer):
             self.model.save(output_dir)
 
         torch.save(self.args, os.path.join(output_dir, "training_args.bin"))
-
-        if is_deepspeed_zero3_enabled():
-            if state_dict is None:
-                state_dict = self.model.state_dict()
-            prefix = 'model.'
-            assert all(k.startswith(prefix) for k in state_dict.keys()), list(state_dict.keys())
-            state_dict = {k[len(prefix):]: v for k, v in state_dict.items()}
-            lora_state_dict = get_peft_model_state_dict(self.model.model, state_dict)
-            if self.args.process_index <= 0:
-                torch.save(lora_state_dict, os.path.join(output_dir, "adapter_model.bin"))
-                print(f"Save adapter model at {output_dir}")
-
-
     def compute_loss(self, model, inputs, return_outputs=False):
         outputs = model(**inputs)
         loss = outputs.loss
